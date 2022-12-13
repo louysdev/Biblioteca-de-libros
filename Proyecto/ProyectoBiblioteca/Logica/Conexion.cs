63549400154788b1bfd01fd5aa354a2c44c252a0
LouysDev <louysdev@gmail.com>
2022-12-13 09:18:31 -0400

    Subiendo proyecto

---
+++ Proyecto/ProyectoBiblioteca/Logica/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoBiblioteca.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=DESKTOP-JK6DAH1;Initial Catalog=DB_BIBLIOTECA;Integrated Security=True";
+    }
+}
